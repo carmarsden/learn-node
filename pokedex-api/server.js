cd90caaa2120fada80dd22499a284af1db054ca5
Caroline Marsden <car.marsden@gmail.com>
2019-07-05 16:27:29 -0400

    Add initial pokedex endpoints

--- pokedex-api/server.js
+++ pokedex-api/server.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const morgan = require('morgan')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
+console.log(process.env.API_TOKEN);
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-    res.send('Hello, world!')
-})
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+app.get('/types', handleGetTypes);
 
-const PORT = 8000
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+};
+app.get('/pokemon', handleGetPokemon);
+
+
+const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
